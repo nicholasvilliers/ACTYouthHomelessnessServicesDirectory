e61be596025720ea588943e540580439cd419ecc
nicholasvilliers <47583787+nicholasvilliers@users.noreply.github.com>
2019-09-16 21:57:35 +1000

    commit selected files

--- ACT Youth Homelessness Services Directory/Controllers/HomeController.cs
+++ ACT Youth Homelessness Services Directory/Controllers/HomeController.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+/*controllers added for contact form*/
+using ACT_Youth_Homelessness_Services_Directory.Models;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
 
 namespace ACT_Youth_Homelessness_Services_Directory.Controllers
 {
@@ -26,5 +32,79 @@ namespace ACT_Youth_Homelessness_Services_Directory.Controllers
 
             return View();
         }
+        public ActionResult Services()
+        {
+            ViewBag.Message = "Services page.";
+
+            return View();
+        }
+        public ActionResult legalfinancial()
+        {
+            ViewBag.Message = "Legal and Financial Services.";
+                return View();
+        }
+        public ActionResult shelter()
+        {
+            ViewBag.Message = "Legal and Financial Services.";
+            return View();
+        }
+        public ActionResult healthwellbeing()
+        {
+            ViewBag.Message = "Health and Wellbeing Services.";
+            return View();
+        }
+        public ActionResult jobsemployment()
+        {
+            ViewBag.Message = "Jobs and Employment Services.";
+            return View();
+        }
+        public ActionResult diversityinclusion()
+        {
+            ViewBag.Message = "Diversity and Inclusion Services.";
+            return View();
+        }
+        public ActionResult familycommunity()
+        {
+            ViewBag.Message = "Family and Community Services";
+            return View();
+        }
+        public ActionResult mealsandfood()
+        {
+            ViewBag.Message = "Meals and Food Services.";
+            return View();
+        }
+        public ActionResult editServices()
+        {
+            ViewBag.Message = "editServices";
+            return View();
+        }
+        public ActionResult belconnen()
+        {
+            ViewBag.Message = "belconnen";
+            return View();
+        }
+        public ActionResult gungahlin()
+        {
+            ViewBag.Message = "gungahlin";
+            return View();
+        }
+        public ActionResult woden()
+        {
+            ViewBag.Message = "woden";
+            return View();
+        }
+        public ActionResult civic()
+        {
+            ViewBag.Message = "civic";
+            return View();
+        }
+        public ActionResult tuggeranong()
+        {
+            ViewBag.Message = "tuggeranong";
+            return View();
+        }
+
+
     }
+
 }
---
+++ ACT Youth Homelessness Services Directory/Models/ServicesClass.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ACT_Youth_Homelessness_Services_Directory.Models
+{
+    public class ServicesClass
+    {
+        /*Service details*/
+        public int ID { get; set; }
+        public string ServiceName { get; set; }
+        public string Description { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string Longitude { get; set; }
+        public string Latitude { get; set; }
+        public string GeoTag { get; set; }
+        public string Email { get; set; }
+        public string OpeningHours { get; set; }
+        /*category types*/
+        public bool Shelter { get; set; }
+        public bool Food { get; set; }
+        public bool JobsEmployment { get; set; }
+        public bool FamilyCommunity { get; set; }
+        public bool LegalFinancial { get; set; }
+        public bool DiversityInclusion { get; set; }
+        public bool HealthWellbeing { get; set; }
+        /*service locations*/
+        public bool Belconnen { get; set; }
+        public bool Gungahlin { get; set; }
+        public bool Woden { get; set; }
+        public bool Tuggeranong { get; set; }
+        public bool Civic { get; set; }
+        /*special access notes*/
+        public string DisabledAccess { get; set; }
+        public string WhoCanAccess { get; set;}
+        public string ReferralRequired { get; set; }
+        public string AgeRange { get; set; }
+        public string Cost { get; set; }
+        public string Lnaguages { get; set; }
+
+    }
+}
